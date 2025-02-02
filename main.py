f8d64162bd382c068b706cb8562fc8fa8216aa37
edusalam <eduarfernando.salcedo@gmail.com>
2025-02-02 05:19:14 -0500

    actualizacion

---
+++ main.py
@@ -0,0 +1,20 @@
+#SINTAXIS BUCLE FOR:
+'''
+cursos = ['ia', 'bigdata', 'sql', 'seo']
+for x in cursos:
+    print(x)
+'''
+#SINTAXIS BUCLE while
+'''
+i = 1
+print('insertando pin...')
+while i < 3:
+    print('error. siguie intentando insertar el pin... tienes ', 3 - i, 'intentos.')
+    i += 1
+    print('has superado los 3 intenos.. llamadn a @policia...') 
+  
+'''
+i = 1
+while i > 10:
+    print(i)
+    i += 10
